f376e779f713e5523d8757b9d9f1652087ec6dc3
Jack Cannon <jc2@quizor2.cs.pdx.edu>
2015-11-29 00:16:33 -0800

    Initial header

---
+++ prog4.h
@@ -0,0 +1,88 @@
+//Jack Cannon
+//CS 163
+//Program 4
+//prog4.h
+//
+//This file contains the prototypes and definitions for the classes, structs
+//and methods used in the ADTs
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+class concept
+{
+    public:
+        concept();
+        ~concept();
+        int set_concept(char new_concept[]);
+        int set_structure(char new_structure[]);
+        int add_advantage(char new_advantage[]);
+        int add_disadvantage(char new_disadvantage[]);
+        int copy_concept(concept & copy_from);
+        int display();
+
+    private:
+        char * concept_name;
+        char * structure_name;
+        char ** advantage_list;
+        char ** disadvantage_list;
+
+};
+
+
+struct node
+{
+    concept data;
+    int traverse;
+    node * left;
+    node * right;
+};
+
+
+class tree
+{
+    public:
+        tree();
+        ~tree();
+        int insert(concept & to_insert);
+        int search(char concept_name[]);
+        int retrieve(concept & to_fill, char concept_name[]);
+        int remove_concept(char concept_name[]);
+        int remove_structure(char structure_name[]);
+        int destroy();
+        int display_in_order();
+        int hash_function(char concept_name[]);
+
+    private:
+        node * root;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
