3bd1ac2729188158c109bdae254a4be1e532ba8c
Jack Cannon <jc2@quizor2.cs.pdx.edu>
2015-11-30 13:31:57 -0800

    Added comments and made styling changes

--- prog4.cpp
+++ prog4.cpp
@@ -13,5 +13,45 @@
 
 int main()
 {
+    //This section tests the concept class
+
+    int len = 5;//list_length
+    char con1[20] = "Linear";//Concept name
+    char st1[20] = "LLL";//Data structure name
+    char adv[5][20] = {"Fast", "Remove at head", "Stretchable"};//Advantages
+    char disadv[5][20] = {"Memory", "Hard to sort", "Hard to search"};//Disadvantages
+
+    concept test1(len), test2(len);
+
+    //Should return 0 - NULL data members
+    cout << test1.display() << endl;
+
+    //Set concept and data structure data members
+    cout << test1.set_concept(con1);
+    cout << test1.set_structure(st1);
+    cout << endl;
+
+
+    //Disadvantages not getting displayed and bullet
+    //numbering off!!
+    //
+    //
+    //
+    //Set advantages and disadvantages
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << test1.add_advantage(adv[i]) << ", ";
+        cout << test1.add_disadvantage(disadv[i]);
+        cout << endl;
+    }
+
+    //Should be successful display of the above data mambers
+    cout << "display() result: " << test1.display() << endl;
+
+    //Copy contents and display new object with copied data
+    //Should match first object
+    cout << "copy_concept(test1) result: " << test2.copy_concept(test1) << endl;
+    cout << "display() result: " << test2.display() << endl;
+
     return 0;
 }
